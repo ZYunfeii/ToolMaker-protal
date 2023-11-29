4d8b7b6265ab13c16436da76b0dd9dee207d3eed
Y. F. Zhang <yunfei_z@buaa.edu.cn>
2023-11-29 20:23:28 +0800

    feat: file upload and download

--- js/base.js
+++ js/base.js
@@ -29,3 +29,52 @@ function initHtml(jsonName) {
             
         });
 }
+
+function deleteFile(fileName) {
+    fetch(`http://localhost:8080/delete/${fileName}`, { method: 'DELETE', credentials: 'include' })
+        .then(response => {
+            // 根据需要处理响应
+            if (response.ok) {
+                fetchFileList(); // 重新获取并显示更新后的文件列表
+            }
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+
+function displayFiles(fileList) {
+    const container = document.getElementById('file-list');
+    container.innerHTML = ''; // 清空旧的内容
+
+    fileList.data.forEach(file => {
+        console.log(file);
+        const fileElement = document.createElement('div');
+        fileElement.textContent = file.fileName;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '删除';
+        deleteButton.onclick = function() {
+            deleteFile(file.fileName);
+        };
+
+        const downloadButton = document.createElement('a');
+        downloadButton.textContent = '下载';
+        downloadButton.href = `http://localhost:8080/download/${file.fileName}`;
+        downloadButton.download = file.fileName;
+
+        fileElement.appendChild(deleteButton);
+        fileElement.appendChild(downloadButton);
+        container.appendChild(fileElement);
+    });
+}
+
+function fetchFileList() {
+    fetch('http://localhost:8080/filelist',{credentials: 'include'})
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            // 处理文件列表
+            displayFiles(data);
+        })
+        .catch(error => console.error('Error:', error));
+}
